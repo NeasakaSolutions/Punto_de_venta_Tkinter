6d92f21b32a341d83afd40a833bd13fcfccff024
Ubaldo <satou.matsuzaka.3443@gmail.com>
2025-05-15 17:07:08 -0600

    Se modifico el archivo para mostrar los datos en la ventana

--- logica/mostrar_usuario.py
+++ logica/mostrar_usuario.py
@@ -1,17 +1,26 @@
 # Importaciones:
 from base_datos.conexion import conectar
 
-def mostrar_usuario():
+def mostrar_usuario(parent):
     """Funcio que muestra los usuarios atravez de consultas"""
 
     # conexion a la bd
     mi_conexion = conectar()
     # Crear el cursor
     mi_cursor = mi_conexion.cursor()
+    
+    # Limpiar el treeview
+    registro = parent.tree_lista_usuarios.get_children()
+    for elementos in registro:
+        parent.tree_lista_usuarios.delete(elementos)
+
     # Crear la cosulta
     mi_cursor.execute("SELECT * FROM Usuarios")
     datos_usuarios = mi_cursor.fetchall()
-    print(datos_usuarios)
+
+    # insertar las filas en el treeview
+    for fila in datos_usuarios:
+        parent.tree_lista_usuarios.insert('', 0, fila[0], values = (fila[0], fila[1], fila[2], fila[3]))
 
     # Aplicar cambios
     mi_conexion.commit()
