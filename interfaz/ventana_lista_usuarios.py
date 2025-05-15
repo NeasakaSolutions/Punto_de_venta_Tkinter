0b584dbd6b09a6fda49499aa78962fcb641c3869
Ubaldo <satou.matsuzaka.3443@gmail.com>
2025-05-15 17:41:13 -0600

    Se agregaron mas disenios para la ventana

--- interfaz/ventana_lista_usuarios.py
+++ interfaz/ventana_lista_usuarios.py
@@ -15,7 +15,7 @@ def ventana_lista_usuarios(parent):
     parent.frame_lista_usuarios.grid(row = 0, column = 1, columnspan = 2, sticky = "NSEW")
 
     # Configurar el diseño de la ventana
-    lblframe_botones_lista_usuarios = LabelFrame(master = parent.frame_lista_usuarios)
+    lblframe_botones_lista_usuarios = tb.LabelFrame(master = parent.frame_lista_usuarios)
     lblframe_botones_lista_usuarios.grid(row = 0, column = 0, padx = 5, pady = 5, sticky = "NSEW")
 
     # Botón para agregar un nuevo usuario
@@ -34,15 +34,15 @@ def ventana_lista_usuarios(parent):
     btn_eliminar_lista_usuarios.grid(row = 0, column = 2, padx = 10, pady = 10)
 
     # Barra de búsqueda
-    lblframe_busqueda_lista_usuarios = LabelFrame(master = parent.frame_lista_usuarios)
+    lblframe_busqueda_lista_usuarios = tb.LabelFrame(master = parent.frame_lista_usuarios)
     lblframe_busqueda_lista_usuarios.grid(row = 1, column = 0, padx= 5, pady = 5, sticky = "NSEW",)
 
     # Etiqueta para la barra de búsqueda
-    ent_buscar_lista_usuarios = Entry(master = lblframe_busqueda_lista_usuarios, width = 100)
+    ent_buscar_lista_usuarios = tb.Entry(master = lblframe_busqueda_lista_usuarios, width = 98)
     ent_buscar_lista_usuarios.grid(row = 0, column = 0, padx = 10, pady = 10)
 
     # Botón para buscar un usuario
-    lblframe_tree_lista_usuarios = LabelFrame(master = parent.frame_lista_usuarios)
+    lblframe_tree_lista_usuarios = tb.LabelFrame(master = parent.frame_lista_usuarios)
     lblframe_tree_lista_usuarios.grid(row = 2, column = 0, padx= 5, pady = 5, sticky = "NSEW",)
 
     # Crear columnas:
@@ -51,13 +51,13 @@ def ventana_lista_usuarios(parent):
     # Crear el Treeview para mostrar la lista de usuarios
     parent.tree_lista_usuarios = ttk.Treeview(master = lblframe_tree_lista_usuarios, height = 17, 
                                               columns = columnas ,show = "headings", 
-                                              bootstyle = "dark")
+                                              bootstyle = "success")
     
     parent.tree_lista_usuarios.grid(row = 0, column = 0, padx = 10, pady = 10)
 
     # Crear las cabeceras
     parent.tree_lista_usuarios.heading('codigo', text = 'Codigo', anchor = W)
-    parent.tree_lista_usuarios.heading('nombre', text = 'Usuario', anchor = W)
+    parent.tree_lista_usuarios.heading('nombre', text = 'Nombre', anchor = W)
     parent.tree_lista_usuarios.heading('clave', text = 'Clave', anchor = W)
     parent.tree_lista_usuarios.heading('rol', text = 'Rol', anchor = W)
 
@@ -65,11 +65,15 @@ def ventana_lista_usuarios(parent):
     parent.tree_lista_usuarios.column('codigo', width = 100)
     parent.tree_lista_usuarios.column('nombre', width = 300)
     parent.tree_lista_usuarios.column('clave', width = 100)
-    parent.tree_lista_usuarios.column('rol', width = 150)
+    parent.tree_lista_usuarios.column('rol', width = 200)
+
+    # Configurar las columnas a mostrar:
+    parent.tree_lista_usuarios['displaycolumns'] = ('codigo', 'nombre', 'rol')
+
 
     # Crear el scrollbar
     tree_scroll = tb.Scrollbar(master = lblframe_tree_lista_usuarios, bootstyle = "success-round")
-    tree_scroll.grid(row = 0, column = 1, padx = 10, pady = 10)
+    tree_scroll.grid(row = 0, column = 1, padx = 5, pady = 10)
 
     # Configurar el scrollbar
     tree_scroll.config(command = parent.tree_lista_usuarios.yview)
