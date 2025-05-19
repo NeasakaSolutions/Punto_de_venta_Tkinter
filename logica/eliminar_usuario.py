52e2530a0e2f4ffb1b8038728a474fa299c307ed
Ubaldo <satou.matsuzaka.3443@gmail.com>
2025-05-18 21:11:44 -0600

    Se agrego el manejo de errores

--- logica/eliminar_usuario.py
+++ logica/eliminar_usuario.py
@@ -13,27 +13,30 @@ def eliminar_usuario(parent):
     valor_usuario_seleccionado_eliminar = parent.tree_lista_usuarios.item(usuario_seleccionado_eliminar,
                                             'values' )
     
-    if valor_usuario_seleccionado_eliminar != '':
-        respuesta = messagebox.askquestion('Eliminando usuario', 
-                                           '¿Esta seguro de eliminar el usuario seleccionado?')
-        if respuesta == 'yes':
-            # conexion a la bd
-            mi_conexion = conectar()
-            # Crear el cursor
-            mi_cursor = mi_conexion.cursor()
+    try:
+        if valor_usuario_seleccionado_eliminar != '':
+            respuesta = messagebox.askquestion('Eliminando usuario', 
+                                            '¿Esta seguro de eliminar el usuario seleccionado?')
+            if respuesta == 'yes':
+                # conexion a la bd
+                mi_conexion = conectar()
+                # Crear el cursor
+                mi_cursor = mi_conexion.cursor()
 
-            # Crear la consulta
-            mi_cursor.execute("DELETE FROM Usuarios WHERE Codigo =" 
-                              + str(valor_usuario_seleccionado_eliminar[0]))
+                # Crear la consulta
+                mi_cursor.execute("DELETE FROM Usuarios WHERE Codigo =" 
+                                + str(valor_usuario_seleccionado_eliminar[0]))
 
-            # Aplicar cambios
-            mi_conexion.commit()
-            messagebox.showinfo('Eliminando usuario', 'Registro eliminado correctamente')
-            buscar_usuario(parent)
-            # Cerrar conexion
-            mi_conexion.close()
+                # Aplicar cambios
+                mi_conexion.commit()
+                messagebox.showinfo('Eliminando usuario', 'Registro eliminado correctamente')
+                buscar_usuario(parent)
+                # Cerrar conexion
+                mi_conexion.close()
 
-        else:
-            messagebox.showerror('Eliminando usuario', 'Eliminacion cancelada')
+            else:
+                messagebox.showerror('Eliminando usuario', 'Eliminacion cancelada')
+    except:
+        messagebox.showerror('Eliminando usuario', 'Ocurrio un error')
 
 
