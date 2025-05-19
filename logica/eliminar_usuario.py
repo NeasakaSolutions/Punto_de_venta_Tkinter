00ae860d230f29fa1afc921dd33ab9b14da127a3
Ubaldo <satou.matsuzaka.3443@gmail.com>
2025-05-18 18:54:46 -0600

    Se agrega la funcion de eliminar usuarios

---
+++ logica/eliminar_usuario.py
@@ -0,0 +1,39 @@
+# Importaciones
+from tkinter import messagebox
+from base_datos.conexion import conectar
+
+def eliminar_usuario(parent):
+    """Funcion que contiene la configuracion para la eliminacion de usuarios"""
+
+    # Importaciones perezosas
+    from logica.mostrar_usuario import mostrar_usuarios
+
+
+    usuario_seleccionado_eliminar = parent.tree_lista_usuarios.focus()
+    valor_usuario_seleccionado_eliminar = parent.tree_lista_usuarios.item(usuario_seleccionado_eliminar,
+                                            'values' )
+    
+    if valor_usuario_seleccionado_eliminar != '':
+        respuesta = messagebox.askquestion('Eliminando usuario', 
+                                           '¿Esta seguro de eliminar el usuario seleccionado?')
+        if respuesta == 'yes':
+            # conexion a la bd
+            mi_conexion = conectar()
+            # Crear el cursor
+            mi_cursor = mi_conexion.cursor()
+
+            # Crear la consulta
+            mi_cursor.execute("DELETE FROM Usuarios WHERE Codigo =" 
+                              + str(valor_usuario_seleccionado_eliminar[0]))
+
+            # Aplicar cambios
+            mi_conexion.commit()
+            messagebox.showinfo('Eliminando usuario', 'Registro eliminado correctamente')
+            mostrar_usuarios(parent)
+            # Cerrar conexion
+            mi_conexion.close()
+
+        else:
+            messagebox.showerror('Eliminando usuario', 'Eliminacion cancelada')
+
+
