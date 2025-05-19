11450261c2264b19a97431bad2458900ffab76ba
Ubaldo <satou.matsuzaka.3443@gmail.com>
2025-05-18 23:06:24 -0600

    Se agrego la opcion de productos

--- interfaz/interfaz_usuarios/ventana_menu.py
+++ interfaz/interfaz_usuarios/ventana_menu.py
@@ -8,6 +8,7 @@ def ventana_menu(parent):
 
     # Importaciones perezosas:
     from interfaz.interfaz_usuarios.ventana_lista_usuarios import ventana_lista_usuarios
+    from interfaz.interfaz_productos.ventana_lista_productos import ventana_lista_productos
 
     # Configuración de la ventana
     parent.frame_left = Frame(master = parent, width = 200)
@@ -19,10 +20,17 @@ def ventana_menu(parent):
     lbl_prueba.grid(row = 0, column = 0, pady = 10, padx = 10)
 
     # Configuración de los botones del menú
+    # Boton usuarios
     btn_usuarios = Button(master = parent.frame_left, text = "Usuarios", width = 15, height = 2,
                           # Se pasan los argumentos a la función ventana_lista_usuarios
                           command = lambda: ventana_lista_usuarios(parent))
     btn_usuarios.grid(row = 0, column = 0, pady = 10, padx = 10)
+
+    # Boton productos
+    btn_productos = Button(master = parent.frame_left, text = "Productos", width = 15, height = 2,
+                          # Se pasan los argumentos a la función ventana_lista_usuarios
+                          command = lambda: ventana_lista_productos(parent))
+    btn_productos.grid(row = 1, column = 0, pady = 10, padx = 10)
 
     
 
