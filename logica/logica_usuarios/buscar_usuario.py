330bf6c824464ea253607ec6c78ce0a3d3268900
Ubaldo <satou.matsuzaka.3443@gmail.com>
2025-05-18 23:29:11 -0600

    Manejo de errores

--- logica/logica_usuarios/buscar_usuario.py
+++ logica/logica_usuarios/buscar_usuario.py
@@ -1,30 +1,35 @@
 # Importaciones:
+from tkinter import messagebox
 from base_datos.conexion import conectar
 
 def buscar_usuario(parent, event = None):
     """Funcion que muestra los usuarios atravez de consultas"""
 
-    # conexion a la bd
-    mi_conexion = conectar()
-    # Crear el cursor
-    mi_cursor = mi_conexion.cursor()
+    try:
+        # conexion a la bd
+        mi_conexion = conectar()
+        # Crear el cursor
+        mi_cursor = mi_conexion.cursor()
+        
+        # Limpiar el treeview
+        registro = parent.tree_lista_usuarios.get_children()
+        for elementos in registro:
+            parent.tree_lista_usuarios.delete(elementos)
+
+        # Crear la cosulta
+        mi_cursor.execute("SELECT * FROM Usuarios WHERE Nombre LIKE ? ORDER BY Clave DESC",
+                        (parent.ent_buscar_lista_usuarios.get() + '%',))
+        datos_usuarios = mi_cursor.fetchall()
+
+        # insertar las filas en el treeview
+        for fila in datos_usuarios:
+            parent.tree_lista_usuarios.insert('', 0, fila[0], values = (fila[0], fila[1], fila[2], fila[3]))
+
+        # Aplicar cambios
+        mi_conexion.commit()
+        # Cerrar conexion
+        mi_conexion.close()
     
-    # Limpiar el treeview
-    registro = parent.tree_lista_usuarios.get_children()
-    for elementos in registro:
-        parent.tree_lista_usuarios.delete(elementos)
-
-    # Crear la cosulta
-    mi_cursor.execute("SELECT * FROM Usuarios WHERE Nombre LIKE ? ORDER BY Clave DESC",
-                      (parent.ent_buscar_lista_usuarios.get() + '%',))
-    datos_usuarios = mi_cursor.fetchall()
-
-    # insertar las filas en el treeview
-    for fila in datos_usuarios:
-        parent.tree_lista_usuarios.insert('', 0, fila[0], values = (fila[0], fila[1], fila[2], fila[3]))
-
-    # Aplicar cambios
-    mi_conexion.commit()
-    # Cerrar conexion
-    mi_conexion.close()
+    except:
+        messagebox.showerror('Buscar usuario', 'Ocurrio un error')
 
--- logica/logica_usuarios/correlativos_usuarios.py
+++ logica/logica_usuarios/correlativos_usuarios.py
@@ -1,33 +1,38 @@
 # Importaciones:
 from tkinter import NORMAL
+from tkinter import messagebox
 from base_datos.conexion import conectar
 
 def correlativo_usuarios(parent):
     """Funcion que ordena las id de los usuarios"""
 
-     # conexion a la bd
-    mi_conexion = conectar()
-    # Crear el cursor
-    mi_cursor = mi_conexion.cursor()
+    try:
+        # conexion a la bd
+        mi_conexion = conectar()
+        # Crear el cursor
+        mi_cursor = mi_conexion.cursor()
 
-    # Crear la cosulta
-    mi_cursor.execute("SELECT MAX(Codigo) FROM Usuarios ORDER BY Clave DESC")
-    correlativo_usuarios = mi_cursor.fetchone()
+        # Crear la cosulta
+        mi_cursor.execute("SELECT MAX(Codigo) FROM Usuarios ORDER BY Clave DESC")
+        correlativo_usuarios = mi_cursor.fetchone()
 
-    for datos in correlativo_usuarios:
-        if datos == None:
-            parent.nuevo_correlativo_usuario = (int(1))
-            parent.ent_codigo_nuevo_usuario.config(state = NORMAL)
-            parent.ent_codigo_nuevo_usuario.insert(0, parent.nuevo_correlativo_usuario)
-            parent.ent_codigo_nuevo_usuario.config(state = 'readonly')
-        else:
-            parent.nuevo_correlativo_usuario = (int(datos) + 1)
-            parent.ent_codigo_nuevo_usuario.config(state = NORMAL)
-            parent.ent_codigo_nuevo_usuario.insert(0, parent.nuevo_correlativo_usuario)
-            parent.ent_codigo_nuevo_usuario.config(state = 'readonly')
+        for datos in correlativo_usuarios:
+            if datos == None:
+                parent.nuevo_correlativo_usuario = (int(1))
+                parent.ent_codigo_nuevo_usuario.config(state = NORMAL)
+                parent.ent_codigo_nuevo_usuario.insert(0, parent.nuevo_correlativo_usuario)
+                parent.ent_codigo_nuevo_usuario.config(state = 'readonly')
+            else:
+                parent.nuevo_correlativo_usuario = (int(datos) + 1)
+                parent.ent_codigo_nuevo_usuario.config(state = NORMAL)
+                parent.ent_codigo_nuevo_usuario.insert(0, parent.nuevo_correlativo_usuario)
+                parent.ent_codigo_nuevo_usuario.config(state = 'readonly')
 
-    # Aplicar cambios
-    mi_conexion.commit()
-    # Cerrar conexion
-    mi_conexion.close()
+        # Aplicar cambios
+        mi_conexion.commit()
+        # Cerrar conexion
+        mi_conexion.close()
+
+    except:
+        messagebox.showerror('Correlativo usuarios', 'Ocurrio un error')
 
